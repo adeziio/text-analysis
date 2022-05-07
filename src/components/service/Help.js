a74d62f5721ff2dba93a177b1a50d943f0723663
Aden <adeziio@yahoo.com>
2022-05-07 03:32:28 -0400

    add help description for language detection

--- src/components/service/Help.js
+++ src/components/service/Help.js
@@ -22,6 +22,10 @@ export default class Help extends Component {
                     <span style={{ fontWeight: "bold" }}>Summarize Text </span>
                     is an automatic summarization process to create a subset that represents the most important or relevant information within the original content.
                 </Typography>
+                <Typography variant="p" color="text.primary" fontSize="1rem" display="block" margin="1rem" >
+                    <span style={{ fontWeight: "bold" }}>Language Detection </span>
+                    is the problem of determining which natural language given content is in.
+                </Typography>
             </>
         )
     }
